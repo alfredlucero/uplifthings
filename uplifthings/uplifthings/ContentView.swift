6dd185c16db2026fd91550e21a87cd2a9dbb9f3e
Alfred Lucero <alfred.lucero@ucla.edu>
2021-10-20 01:49:20 -0700

    Tweaked layout and added shadows

--- uplifthings/uplifthings/ContentView.swift
+++ uplifthings/uplifthings/ContentView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .center) {
+        VStack {
             VStack(alignment: .leading, spacing: 8.0) {
-                   
                 Circle()
                     .frame(width: 40.0, height: 40.0)
                     .foregroundColor(Color(hue: 0.139, saturation: 0.43, brightness: 0.897))
@@ -27,17 +26,17 @@ struct ContentView: View {
             .padding(.all)
             .background(Color(hue: 0.488, saturation: 0.469, brightness: 0.774))
             .cornerRadius(20.0)
-            
+            .shadow(color: Color(hue: 0.488, saturation: 0.469, brightness: 0.774).opacity(0.5), radius: 20, x: 0, y: 8)
+                
             Text("What uplifts you today?")
                 .font(.title3)
                 .bold()
                 .foregroundColor(Color(hue: 0.488, saturation: 0.531, brightness: 0.488))
-                .padding(.all)
-                
+                .padding(.top)
+                .shadow(color: Color(hue: 0.488, saturation: 0.531, brightness: 0.488).opacity(0.2), radius: 5, x: 0, y: 8)
             
             Spacer()
         }
-     
     }
 }
 
