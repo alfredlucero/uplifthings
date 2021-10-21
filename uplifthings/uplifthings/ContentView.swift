416aeb36d5065f8f38d42919a264214d103c88b3
Alfred Lucero <alfred.lucero@ucla.edu>
2021-10-21 00:16:54 -0700

    Added 10 text editors with state binding

--- uplifthings/uplifthings/ContentView.swift
+++ uplifthings/uplifthings/ContentView.swift
@@ -7,37 +7,57 @@
 
 import SwiftUI
 
+struct Uplifthing: Identifiable {
+    let id: Int
+    var thing: String
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            VStack(alignment: .leading, spacing: 8.0) {
-                Circle()
-                    .frame(width: 40.0, height: 40.0)
-                    .foregroundColor(Color(hue: 0.139, saturation: 0.43, brightness: 0.897))
-               
-                Text("Uplifthings")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                Text("Write 10 things to uplift your soul everyday")
-                    .foregroundColor(.white)
-                    .font(.title2)
-            }
-            .padding(.all)
-            .background(Color(hue: 0.488, saturation: 0.469, brightness: 0.774))
-            .cornerRadius(20.0)
-            .shadow(color: Color(hue: 0.488, saturation: 0.469, brightness: 0.774).opacity(0.5), radius: 20, x: 0, y: 8)
-                
-            Text("What uplifts you today?")
-                .font(.title3)
-                .bold()
-                .foregroundColor(Color(hue: 0.488, saturation: 0.531, brightness: 0.488))
-                .padding(.top)
-                .shadow(color: Color(hue: 0.488, saturation: 0.531, brightness: 0.488).opacity(0.2), radius: 5, x: 0, y: 8)
-            
-            Spacer()
-        }
+    @State private var uplifthings: [Uplifthing] = Array(repeating: Uplifthing(id: 0, thing: ""), count: 10).enumerated().map { (index, element) in
+        return Uplifthing(id: index + 1, thing: "")
     }
+
+    var body: some View {
+            VStack {
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Circle()
+                        .frame(width: 40.0, height: 40.0)
+                        .foregroundColor(Color(hue: 0.139, saturation: 0.43, brightness: 0.897))
+                   
+                    Text("Uplifthings")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("Write 10 things to uplift your soul everyday")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                }
+                .padding(.all)
+                .background(Color(hue: 0.488, saturation: 0.469, brightness: 0.774))
+                .cornerRadius(20.0)
+                .shadow(color: Color(hue: 0.488, saturation: 0.469, brightness: 0.774).opacity(0.5), radius: 20, x: 0, y: 5)
+                
+                Text("What uplifts you today?")
+                    .font(.title2)
+                    .bold()
+                    .padding()
+                    .shadow(color: Color(hue: 0.488, saturation: 0.531, brightness: 0.488).opacity(0.2), radius: 5, x: 0, y: 8)
+                    .foregroundColor(Color(hue: 0.488, saturation: 0.531, brightness: 0.488))
+
+         
+                List($uplifthings) { $uplifthing in
+                    HStack {
+                        Text("\($uplifthing.id).")
+                            .padding(.horizontal, 5)
+                        TextEditor(text: $uplifthing.thing)
+                            .multilineTextAlignment(.leading)
+                            .lineSpacing(10.0)
+                    }
+                    .foregroundColor(Color(hue: 0.488, saturation: 0.531, brightness: 0.488))
+                }
+                .padding(.vertical)
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
